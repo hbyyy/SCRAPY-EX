ff6b3d0ba7323868453779825dbaf101bf1b7461
hbyyy <qjaduddl94@gmail.com>
2020-06-23 17:46:18 +0900

    scrapy export

--- doc/scrapy export.md
+++ doc/scrapy export.md
@@ -14,5 +14,55 @@
 
 
 
+### Feed Exports
 
+- [Scrapy 문서 Feed exports](https://docs.scrapy.org/en/latest/topics/feed-exports.html)
+
+
+
+
+#### 출력 형식
+   1. JSON, JSON Lines
+   2. CSV
+   3. XML, Pickle, Marshal
+
+
+
+#### 저장 위치
+   1. Local File System (My PC)
+   2. FTP - (Server)
+   3. S3 - (AWS)
+   4. 기본 콘솔
+
+
+
+#### 방법
+   1. 커맨드 이용
+       - ((--output, -o) , (--output-format, -t)) 
+       - 옵션 설정 예 -> --set=FEED_EXPORT_INDENT = 2
+       
+   2. settings.py 에 설정
+       - 자동으로 저장 (파일명, 형식, 위치)
+
+
+
+**`settings.py`**
+
+```python
+...
+# Export setting
+
+# 파일이름 및 경로
+FEED_URI = 'result.json'
+
+# 파일 형식
+FEED_FORMAT = 'json'
+
+# 출력 인코딩
+FEED_EXPORT_ENCODING = 'utf-8'
+
+# 기본 들여쓰기
+# json 이나 xml 형식일 때만 적용된다
+FEED_EXPORT_INDENT = 2
+```
 
--- section03_5/section03_5/spiders/class03_5.py
+++ section03_5/section03_5/spiders/class03_5.py
@@ -3,9 +3,31 @@ import scrapy
 
 from ..items import ItArticle
 
+# Scrapy Feed Export 실습
+
+"""
+ 출력 형식
+    JSON, JSON Lines
+    CSV
+    XML, Pickle, Marshal
+    
+ 저장 위치
+    Local File System (My PC)
+    FTP - (Server)
+    S3 - (AWS)
+    기본 콘솔
+    
+ 방법
+    1. 커맨드 이용
+        ((--output, -o) , (--output-format, -t)) 
+        옵션 설정 예 -> --set=FEED_EXPORT_INDENT = 2
+        
+    2. settings.py 에 설정
+        자동으로 저장 (파일명, 형식, 위치)
+"""
 
 class TestSpider(scrapy.Spider):
-    name = 'test6'
+    name = 'test7'
     allowed_domains = ['itnews.com']
     start_urls = ['https://itnews.com/']
 
@@ -31,5 +53,4 @@ class TestSpider(scrapy.Spider):
         item['image_url'] = response.xpath('//img[@itemprop="contentUrl"]/@data-original').get()
         item['contents'] = ''.join(response.xpath('//div[@itemprop="articleBody"]/p/text()').getall()).strip()
 
-        print(dict(item))
         yield item
