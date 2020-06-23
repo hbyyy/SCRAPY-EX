5ed3b94c3d94ff03bb55d3079f6aa777ff56ce8a
hbyyy <qjaduddl94@gmail.com>
2020-06-23 14:49:49 +0900

    spider 정리

--- section03_2/section03_2/spiders/class03_2.py
+++ section03_2/section03_2/spiders/class03_2.py
@@ -1,64 +1,10 @@
-# Scrapy spider
-
-
-
-## Spider 의 종류
-
-기본적으로는 scrapy.Spider 클래스를 사용하는데, 이 클래스를 상속받은 4가지 spider도 있다.
-
-- CrawlSpider
-- XMLFeedSpider
-- CSVFeedSpider
-- SitemapSpider
-
-기본적인 Spider 클래스로도 원하는 기능들을 거의 사용 가능하다.
-
-
-
-## Attribute
-
-### name
-
-- spider 의 이름을 정해준다
-- `scrapy crawl <정해준 이름>` 같이 스파이더를 실행할 때 name 에서 정해준 값으로 실행한다.
-
-
-
-### allowed_domain
-
-- 크롤링시 접근할 도메인을 설정해 준다
-
-
-
-### custom_settings
-
-- 스파이더 프로젝트의 세팅을 동적으로 정해준다.
-- settings.py 에 정의되어있는 설정보다 우선적으로 적용된다,
-
-
-
-### start_urls
-
-- 크롤링을 시작할 url을 설정해 준다
-
-
-
-### start_request() 메소드
-
-- start_urls 와 비슷한 기능을 한다( start_urls를 정해주는 방법이 이 메소드를 오버라이딩하는 것의 shortcut이다)
-- 각각의 url마다 다른 처리 메소드를 정의해주고 싶다면 이 메소드를 오버라이딩해서 설정해 준다.
-
-
-
-### Code
-
-```python
+# -*- coding: utf-8 -*-
 
 import scrapy
-# import logging
+
 
 # 파이썬의 로거를 사용할 수도 있다
-# 스크래피 프레임워크에 logging이 정의되어 있는데 굳이 파이썬 패키지를 쓸 필요는 없을듯
+# 스크래피 프레임워크에 logging이 저으이되어 있는데 굳이 파이썬 패키지를 쓸 필요는 없다.
 # logger = logging.getLogger('MyLogger')
 
 
@@ -82,12 +28,10 @@ class TestSpider(scrapy.Spider):
     #     yield scrapy.Request('https://naver.com', self.parse2)
     #     yield scrapy.Request('https://daum.net', self.parse3)
 
-    
     def parse(self, response):
         self.logger.info('Response URL : %s' % response.url)
         self.logger.info('Response Status : %s' % response.status)
-		
-        # url 마다 다르게 처리
+
         if response.url.find('scrapinghub'):
             yield {
                 'sitemap': response.url,
@@ -103,6 +47,3 @@ class TestSpider(scrapy.Spider):
                 'sitemap': response.url,
                 'contents': response.text[:100]
             }
-
-```
-
